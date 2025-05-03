005674dd15417f83b2edde7dfdc97fb061dc4233
TBERT31 <berteau.thomas@hotmail.fr>
2025-05-03 02:50:18 +0200

    Carefull to Identity display in header

--- nextjs-frontend/components/header.tsx
+++ nextjs-frontend/components/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
             <li>
               {isAuthenticated ? (
                 <>
-                  <p className="text-white">Hi {username}</p>
+                  <p className="text-black">Hi {username}</p>
                   <a
                     onClick={() => signOut()}
                     className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent cursor-pointer"
