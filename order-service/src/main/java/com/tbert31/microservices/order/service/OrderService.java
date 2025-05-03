322f96031944878d69173dbb47bbbfe40c9c3ae9
TBERT31 <berteau.thomas@hotmail.fr>
2025-05-03 10:53:04 +0200

    setup order service to send message to apache kafka

--- order-service/src/main/java/com/tbert31/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/tbert31/microservices/order/service/OrderService.java
@@ -2,19 +2,24 @@ package com.tbert31.microservices.order.service;
 
 import com.tbert31.microservices.order.client.InventoryClient;
 import com.tbert31.microservices.order.dto.OrderRequest;
+import com.tbert31.microservices.order.event.OrderPlacedEvent;
 import com.tbert31.microservices.order.model.Order;
 import com.tbert31.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -26,6 +31,11 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode "+ orderRequest.skuCode() + " is out off stock");
         }
