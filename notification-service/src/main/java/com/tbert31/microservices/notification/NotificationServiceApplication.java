d7cd568c6454708a101e1fd4b4305207e8ffab84
TBERT31 <berteau.thomas@hotmail.fr>
2025-05-03 10:57:41 +0200

    Init Notification Service

---
+++ notification-service/src/main/java/com/tbert31/microservices/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tbert31.microservices.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
