6d72168bc8242f2a2fd5bc84785cb5fd578ac2bb
TBERT31 <berteau.thomas@hotmail.fr>
2025-05-04 17:09:08 +0200

    Try make work frontend with k8s cluster

--- k8s/kind/kind-load.sh
+++ k8s/kind/kind-load.sh
@@ -16,20 +16,20 @@ docker pull tbert31/example-archi-ms-order-service:latest
 docker pull tbert31/example-archi-ms-inventory-service:latest
 docker pull tbert31/example-archi-ms-notification-service:latest
 
-kind load docker-image -n microservices mongo:7.0.5
-kind load docker-image -n microservices mysql:8.3.0
-kind load docker-image -n microservices confluentinc/cp-zookeeper:7.5.0
-kind load docker-image -n microservices confluentinc/cp-kafka:7.5.0
-kind load docker-image -n microservices confluentinc/cp-schema-registry:7.5.0
-kind load docker-image -n microservices provectuslabs/kafka-ui:latest
-kind load docker-image -n microservices mysql:8
-kind load docker-image -n microservices quay.io/keycloak/keycloak:26.2.0
-kind load docker-image -n microservices grafana/loki:main
-kind load docker-image -n microservices prom/prometheus:v2.46.0
-kind load docker-image -n microservices grafana/tempo:2.2.2
-kind load docker-image -n microservices grafana/grafana:10.1.0
-kind load docker-image -n microservices tbert31/example-archi-ms-api-gateway:latest
-kind load docker-image -n microservices tbert31/example-archi-ms-product-service:latest
-kind load docker-image -n microservices tbert31/example-archi-ms-order-service:latest
-kind load docker-image -n microservices tbert31/example-archi-ms-inventory-service:latest
-kind load docker-image -n microservices tbert31/example-archi-ms-notification-service:latest
+kind load docker-image -n example-archi-microservices mongo:7.0.5
+kind load docker-image -n example-archi-microservices mysql:8.3.0
+kind load docker-image -n example-archi-microservices confluentinc/cp-zookeeper:7.5.0
+kind load docker-image -n example-archi-microservices confluentinc/cp-kafka:7.5.0
+kind load docker-image -n example-archi-microservices confluentinc/cp-schema-registry:7.5.0
+kind load docker-image -n example-archi-microservices provectuslabs/kafka-ui:latest
+kind load docker-image -n example-archi-microservices mysql:8
+kind load docker-image -n example-archi-microservices quay.io/keycloak/keycloak:26.2.0
+kind load docker-image -n example-archi-microservices grafana/loki:main
+kind load docker-image -n example-archi-microservices prom/prometheus:v2.46.0
+kind load docker-image -n example-archi-microservices grafana/tempo:2.2.2
+kind load docker-image -n example-archi-microservices grafana/grafana:10.1.0
+kind load docker-image -n example-archi-microservices tbert31/example-archi-ms-api-gateway:latest
+kind load docker-image -n example-archi-microservices tbert31/example-archi-ms-product-service:latest
+kind load docker-image -n example-archi-microservices tbert31/example-archi-ms-order-service:latest
+kind load docker-image -n example-archi-microservices tbert31/example-archi-ms-inventory-service:latest
+kind load docker-image -n example-archi-microservices tbert31/example-archi-ms-notification-service:latest
